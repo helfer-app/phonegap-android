623336ad3b9c4f0381b30430993358aba1ef4724
Marcel Caspar <marcel.caspar@sittig.de>
2018-05-16 16:29:54 +0200

    app init

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'landeskinderturnfest-app-android',
+		wp_ws_url : 'https://lktf.turnjugend-frankfurt.de/wp-appkit-api/landeskinderturnfest-app-android',
+		wp_url : 'https://lktf.turnjugend-frankfurt.de',
+		theme : 'q-android-lktf',
+		version : '0.1',
+		app_type : 'phonegap-build',
+		app_title : 'Landeskinderturnfest Helfer App',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'v=c7Wxmb{9Lkb?{m1d^%i~wQqD*5>PlVNH|kJ#^MB:X1=Y.*h8#juFaQV>+w(VpR',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
